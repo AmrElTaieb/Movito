9cf9b5117ba00fa14c50479a86c29edf465a9578
Amr Mohamed Koritem <amr.koritem92@gmail.com>
2019-03-30 08:50:56 -0700

    Init commit - creating project

---
+++ Movito/Movito/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Movito
+//
+//  Created by Amr Mohamed Koritem on 3/30/19.
+//  Copyright © 2019 Amr Mohamed Koritem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Movito/Movito/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Movito
+//
+//  Created by Amr Mohamed Koritem on 3/30/19.
+//  Copyright © 2019 Amr Mohamed Koritem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
